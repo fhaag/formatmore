458f62c3b6d331ce07d335d2c15e5314e950d3e9
Florian Haag <fhaag@users.sourceforge.net>
2021-12-31 00:16:34 +0100

    added initial version of code

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
-# formatmore
+# FormatMore
+
 A library that offers a more versatile alternative to the String.Format method from .NET.
+
+## Quick Start
+
+Download and reference `FormatMore.dll`.
+Add
+
+```csharp
+using FormatMoreUtilities;
+```
+
+(or the appropriate equivalent command in your language of choice) to your file.
+
+Invoke `FormatMore.Format` instead of `string.Format`.
+
+## Full Syntax Specification
+
+The [`String.Format` method of .NET](https://docs.microsoft.com/en-us/dotnet/api/system.string.format) defines a [format item](https://docs.microsoft.com/en-us/dotnet/api/system.string.format#the-format-item) as
+
+    {index[,alignment][:formatString]}
+
+The *FormatMore* library provides an alternative `FormatMore.Format` method that accepts extended format items:
+
+    {index[listFormat][,alignment][:formatString]}
+
+The additional *listFormat* element applies to arguments that can be converted to `System.Collections.IEnumerable`.
+It is defined as follows:
+
+- It starts with an opening square bracket `[`.
+- It may contain any number of *arguments* that are separated with the pipe character `|`.
+- The first argument specifies the item count. It is a positive number (e.g. `15`) or a number range of the form `2..5`. This argument is optional.
+- Any further arguments start with a single identifying character that may be followed by any number of characters.
+- It ends with a closing square bracket `]`.
+
+### Item Count
+
+If the item count is specified as a single integer number, it indicates the maximum number of items to output.
+If the enumerable contains more items, a *more items marker* will be appended.
+
+If the item count is specified as an integer range (e.g. `8..10`), the *upper bound* of the range indicates the maximum number of items to output.
+If the enumerable contains more items, only the number of items specified as the *lower bound* of the range will be output.
+Also, a *more items marker* will be appended.
+
+If no item count is specified, all items from the enumeration will be output.
+
+### Further Arguments
+
+The following table lists additionally supported arguments along with their identifying characters.
+
+| Identifying Character | Meaning |
+| -- | -- |
+| `d` | Specifies the delimiter to use between items of the enumerable. |
+| `D` | Specifies an individual delimiter between items. Use *indexed values* (see below) to indicate which delimiter you would like to define, with the index referring to the delimiters between actually output items.<br />You may specify several `D` arguments to assign delimiters for different indexes. |
+| `e` | If the enumerable contains no items, the alternative text from this argument will be used instead. |
+| `m` | If the enumerable contains more items than can be displayed as per *Item Count*, this string will be appended after the items. |
+| `r` | If the enumerable contains more items than can be displayed as per *Item Count*, this string will be appended as an additional item representing the remainder of the items. |
+
+If any of these arguments appears multiple times, they will be considered in their order of occurrence.
+
+### Indexed Values
+
+An indexed value is specified in the form
+
+    index[/condition]=value
+
+In there:
+
+- *index* is an integer number. Count the index from zero upwards to reference indexes from the start of the collection, and from `-1` downwards to start from the end.
+- *condition* is an optional condition that checks the total number of items available in the source enumerable. The condition consists of one of the operators `<`, `>`, or `=` (if no operator is specified, `=` will be assumed), followed by an integer number that is compared to the total number of items.
+- *value* is the resulting value for the specified index if the condition is met.
+
+## Examples
+
+### Sum
+
+All items from the enumerable are output and joined with the specified delimiter.
+
+```csharp
+var format = "{0[d + ]}";
+
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2 } }));
+// 1 + 2
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2, 4 } }));
+// 1 + 2 + 4
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2, 4, 8, 16 } }));
+// 1 + 2 + 4 + 8 + 16
+```
+
+### Shortened List
+
+The output list is shortened once it exceeds three items.
+
+```csharp
+var format = "{0[3|d;|m...]}";
+
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2 } }));
+// 1;2
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2, 4 } }));
+// 1;2;4
+Console.WriteLine(FormatMore.Format(format,
+  new object[] { new[] { 1, 2, 4, 8, 16 } }));
+// 1;2;4...
+```
+
+### Text Enumeration
+
+The enumeration is properly formatted for a natural language text (depending on which styleguide is used, but that question is out of scope here). Note how the case of two items is treated separately.
+
+```csharp
+var format = "My pizza topping consists of {0[2|d, |rothers|D0/2= and |D-1=, and ]}.";
+var ingredients = new[] { "tomatoes", "mushrooms", "onions", "sliced sausages", "spinach leaves" };
+
+Console.WriteLine(FormatMore.Format(format, new object[] { ingredients.Take(1) }));
+// My pizza topping consists of tomatoes.
+Console.WriteLine(FormatMore.Format(format, new object[] { ingredients.Take(2) }));
+// My pizza topping consists of tomatoes and mushrooms.
+Console.WriteLine(FormatMore.Format(format, new object[] { ingredients.Take(3) }));
+// My pizza topping consists of tomatoes, mushrooms, and others.
+Console.WriteLine(FormatMore.Format(format, new object[] { ingredients.Take(5) }));
+// My pizza topping consists of tomatoes, mushrooms, and others.
+```
+
+### Authors List
+
+Author names are formatted according to the conventions of some widespread styleguides for scientific publications. Up to three authors are spelt out, but for anything above, only the first author and the remark "et al." are output.
+
+```csharp
+var format = "{0[1..3|d, |D0/2= and |D-1=, and |m et al.]}";
+(string GivenName, string Surname)[] authors =
+{
+    ("Eddie", "Example"),
+    ("Sue", "Sample"),
+    ("Bert", "Beispiel"),
+    ("Eduardo", "Ejemplo")
+};
+var formattedAuthors = authors.Select(a => $"{a.GivenName[0]}. {a.Surname}");
+
+Console.WriteLine(FormatMore.Format(format, new object[] { formattedAuthors.Take(1) }));
+// E. Example
+Console.WriteLine(FormatMore.Format(format, new object[] { formattedAuthors.Take(2) }));
+// E. Example and S. Sample
+Console.WriteLine(FormatMore.Format(format, new object[] { formattedAuthors.Take(3) }));
+// E. Example, S. Sample, and B. Beispiel
+Console.WriteLine(FormatMore.Format(format, new object[] { formattedAuthors.Take(4) }));
+// E. Example et al.
+```
+
+## License
+
+The *FormatMore* library is published under the terms of the MIT license.
---
+++ src/FormatMore.Test/EnumerableTest.cs
@@ -0,0 +1,171 @@
+﻿/*
+Copyright (c) 2021 Florian Haag
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+#nullable enable
+
+namespace FormatMoreUtilities.Test
+{
+    public sealed class EnumerableTest
+    {
+        #region infrastructure
+
+        private void TestInput(string expected, string format, params object[] args) => TestInput(expected, null, format, args);
+
+		private void TestInput(string expected, IFormatProvider? provider, string format, params object[] args)
+		{
+			var formatMoreResult = FormatMore.Format(provider, format, args);
+
+			formatMoreResult.Should().Be(expected);
+		}
+
+        #endregion
+
+        #region data
+
+        private static readonly char[] fiveLetters = new[] { 'f', 'B', 'e', 'a', 'V' };
+
+        private static readonly float[] sixFloats = new[] { 3f, 2.5f, 6.71855f, 2332.0059221f, -93.1782f, 0.0035583f };
+
+        #endregion
+
+        [Fact]
+        public void TestEmpty() => TestInput("abc", "a{0[]}bc", new object[] { new string[0] });
+
+        [Fact]
+        public void TestEmptyPlaceholder()
+        {
+            var format = "{0[e(none)]}";
+            TestInput("(none)", format, new object[] { new string[0] });
+            TestInput("abcxyz", format, new object[] { new[] { "abc", "xyz" } });
+        }
+
+        [Fact]
+        public void TestSingleItem() => TestInput("abcdmnf", "abcd{0[]}f", new object[] { new[] { 'm', 'n' } });
+
+        [Fact]
+        public void TestMultiItems() => TestInput("fBeaVxyz", "{0[]}xyz", new object[] { fiveLetters });
+
+        [Fact]
+        public void TestMulti() => TestInput("def;GHI;JK-x,ab", "{1[d;]}-{0[d,]}", new object[] { new[] { "x", "ab" }, new[] { "def", "GHI", "JK" } });
+
+        [Fact]
+        public void TestDelimitedMultiItems() => TestInput("ABCf + B + e + a + VDEF", "ABC{0[d + ]}DEF", new object[] { fiveLetters });
+
+        [Fact]
+        public void TetsDelimiterPrecedence() => TestInput("f+B+e+a+V", "{0[d+|d-]}", new object[] { fiveLetters });
+
+        [Fact]
+        public void TestSpecialDelimiters() => TestInput("Af-B-e+a..VBC", "A{0[d-|D-2=+|D-1=..]}BC", new object[] { fiveLetters });
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("=")]
+        public void TestEqualConditionalDelimiters(string conditionPrefix)
+        {
+            var format = "{0[d+|D-1/" + conditionPrefix + "3=_|D-1=-]}";
+            TestInput("a-b", format, new object[] { new[] { 'a', 'b' } });
+            TestInput("a+b_c", format, new object[] { new[] { 'a', 'b', 'c' } });
+            TestInput("a+b+c-d", format, new object[] { new[] { 'a', 'b', 'c', 'd' } });
+            TestInput("a+b+c+d-e", format, new object[] { new[] { 'a', 'b', 'c', 'd', 'e' } });
+        }
+
+        [Fact]
+        public void TestLessThanConditionalDelimiters()
+        {
+            var format = "{0[d+|D-1/<4=_|D-1=-]}";
+            TestInput("a_b", format, new object[] { new[] { 'a', 'b' } });
+            TestInput("a+b_c", format, new object[] { new[] { 'a', 'b', 'c' } });
+            TestInput("a+b+c-d", format, new object[] { new[] { 'a', 'b', 'c', 'd' } });
+            TestInput("a+b+c+d-e", format, new object[] { new[] { 'a', 'b', 'c', 'd', 'e' } });
+        }
+
+        [Fact]
+        public void TestGreaterThanConditionalDelimiters()
+        {
+            var format = "{0[d+|D-1/>3=_|D-1=-]}";
+            TestInput("a-b", format, new object[] { new[] { 'a', 'b' } });
+            TestInput("a+b-c", format, new object[] { new[] { 'a', 'b', 'c' } });
+            TestInput("a+b+c_d", format, new object[] { new[] { 'a', 'b', 'c', 'd' } });
+            TestInput("a+b+c+d_e", format, new object[] { new[] { 'a', 'b', 'c', 'd', 'e' } });
+        }
+
+        private sealed class FormattingCultures : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new CultureInfo("en-US") };
+                yield return new object[] { new CultureInfo("de-DE") };
+                yield return new object[] { new CultureInfo("zh-Hans") };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Theory]
+        [ClassData(typeof(FormattingCultures))]
+        public void TestDefaultFormatFloats(CultureInfo culture) => TestInput("_" + String.Join(";", sixFloats.Select(f => f.ToString(culture))),
+            culture, "_{0[d;]}", new object[] { sixFloats });
+
+        [Theory]
+        [ClassData(typeof(FormattingCultures))]
+        public void TestCustomFormatFloats(CultureInfo culture) => TestInput("_" + String.Join("/", sixFloats.Select(f => f.ToString("0.##", culture))),
+            culture, "_{0[d/]:0.##}", new object[] { sixFloats });
+
+        [Fact]
+        public void TestCount()
+        {
+            TestInput("rrfB", "rr{0[2]}", new object[] { fiveLetters });
+            TestInput("rrfBe", "rr{0[3]}", new object[] { fiveLetters });
+            TestInput("rrfBeaV", "rr{0[5]}", new object[] { fiveLetters });
+            TestInput("rrfBeaV", "rr{0[6]}", new object[] { fiveLetters });
+        }
+
+        [Fact]
+        public void TestMaxCount()
+        {
+            TestInput("rrfB", "rr{0[2..4]}", new object[] { fiveLetters });
+            TestInput("rrfBeaV", "rr{0[2..6]}", new object[] { fiveLetters });
+        }
+
+        [Fact]
+        public void TestMoreItemsMarker()
+        {
+            TestInput("f+B...", "{0[2|d+|m...]}", new object[] { fiveLetters });
+            TestInput("f+B+e+a+V", "{0[8|d+|m...]}", new object[] { fiveLetters });
+        }
+
+        [Fact]
+        public void TestRemainderItem()
+        {
+            TestInput("f+B+...", "{0[2|d+|r...]}", new object[] { fiveLetters });
+            TestInput("f+B+e+a+V", "{0[8|d+|r...]}", new object[] { fiveLetters });
+        }
+    }
+}
